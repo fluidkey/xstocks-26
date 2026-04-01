9f6233118cb1c8c4cedbf09a849fd8b4ea7297f5
Federico Castelli <fc@oracleofde.fi>
2026-04-01 16:51:52 +0200

    feat: include vault price calculation + apr cumulative

---
+++ backend/src/lambda-functions/fetch-prices/local-development/fetch-prices.local-test.ts
@@ -0,0 +1,21 @@
+import * as lambdaLocal from 'lambda-local';
+import path from 'node:path';
+
+void lambdaLocal.execute({
+  event: {},
+  lambdaPath: path.join(__dirname, '../fetch-prices.lambda.ts'),
+  profilePath: '~/.aws/credentials',
+  profileName: 'fluidkey',
+  region: 'eu-west-1',
+  timeoutMs: 60000,
+  callback: function (err: any, data: any) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  },
+  environment: {
+    PRICES_BUCKET: 'xstocks2026-pricesbucket7bd6c8de-20gavwamlmay',
+  },
+});
